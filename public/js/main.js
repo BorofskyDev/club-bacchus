77b5c4b5fef95590f905dd51bd8fc24f84ac3386
solovyovsdom <joelborofsky@mac.com>
2022-10-07 17:12:25 -0500

    first commit

---
+++ public/js/main.js
@@ -0,0 +1,24 @@
+import '../styles/main.css';
+import '../styles/navbar.css';
+
+const navMenu = document.getElementById('nav-menu');
+const toggleMenu = document.getElementById('toggle-menu');
+const closeMenu = document.getElementById('close-menu');
+
+toggleMenu.addEventListener('click', () => {
+  navMenu.classList.add('show');
+});
+
+toggleMenu.addEventListener('keydown', () => {
+  navMenu.classList.add('show');
+});
+
+closeMenu.addEventListener('click', () => {
+  navMenu.classList.remove('show');
+});
+
+closeMenu.addEventListener('keydown', () => {
+    navMenu.classList.remove('show');
+});
+
+// NAVBAR
