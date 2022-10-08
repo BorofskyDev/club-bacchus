092e6e59734e3525c03da14f54c59ce13a57d70a
solovyovsdom <joelborofsky@mac.com>
2022-10-07 20:36:57 -0500

    added desktop style for first part of abt section

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,7 @@
 import '../styles/main.css';
 import '../styles/navbar.css';
-import '../styles/hero.css'
+import '../styles/hero.css';
+import '../styles/about.css';
 
 const navMenu = document.getElementById('nav-menu');
 const toggleMenu = document.getElementById('toggle-menu');
