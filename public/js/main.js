cfb9f36149aed8d7640aa07f0c0ac4fe1fe43e14
solovyovsdom <joelborofsky@mac.com>
2022-10-08 18:33:15 -0500

    figured out my click function

--- public/js/main.js
+++ public/js/main.js
@@ -11,6 +11,9 @@ const navMenu = document.getElementById('nav-menu');
 const toggleMenu = document.getElementById('toggle-menu');
 const closeMenu = document.getElementById('close-menu');
 
+
+
+
 // NAVBAR
 toggleMenu.addEventListener('click', () => {
   navMenu.classList.add('show');
@@ -30,29 +33,36 @@ closeMenu.addEventListener('keydown', () => {
 
 // CARD
 
+document.addEventListener('click', function (e) {
+  if(e.target.dataset.pay) {
+    handlePayClick(e.target.dataset.pay)
+  }
+})
+
+function handlePayClick(cards) {
+  console.log("okay")
+  // function of openModal() goes here, need to create the function
+}
+
 function getCards() {
   let cardHTML = ``;
 
   cardData.forEach(function (card) {
     cardHTML += `
-    
-    
-      <div class="cta__card">
-          <div class="cta__content">
-            <h2 class="cta__number">${card.id}</h2>
-            <h3 class="cta__title">${card.level}</h3>
-            <p class="cta__text">${card.text}
-            </p>
-            <h4 class="cta__price">Price: &euro;<span>${card.price}</span></h4>
-            <button class="cta__pay"  id='cta-pay'>Pay Now</button>
-          </div>
-          </div>
-          `;
+    <div class="cta__card">
+      <div class="cta__content">
+       <h2 class="cta__number">${card.id}</h2>
+        <h3 class="cta__title">${card.level}</h3>
+        <p class="cta__text">${card.text}</p>
+         <h4 class="cta__price">Price: &euro;<span>${card.price}</span></h4>
+        <button class="cta__pay" data-pay="${card.id}">Pay Now</button>
+      </div>
+    </div>
+            `;
+           
   });
   return cardHTML;
 }
-
-
 
 function render() {
   document.getElementById('cta-container').innerHTML = getCards();
