1606defd060dfc2c06de37cf6e7e5caf9ce31bb0
solovyovsdom <joelborofsky@mac.com>
2022-10-08 16:29:45 -0500

    rendered pay options from array

---
+++ public/js/cardData.js
@@ -0,0 +1,20 @@
+export const cardData = [
+    {
+        id: 1,
+        level:"Basic",
+        text:"1 month access to Club Bacchus",
+        price:500
+    },
+    {
+        id:2,
+        level:"Gold",
+        text:"6 months access to Club Bacchus",
+        price:1000
+    },
+    {
+        id:3,
+        level:"Platinum",
+        text:"1 year access to Club Bacchus",
+        price:2000
+    },
+]   
--- public/js/main.js
+++ public/js/main.js
@@ -5,10 +5,11 @@ import '../styles/about.css';
 import '../styles/amenities.css';
 import '../styles/cta.css';
 
+import { cardData } from './cardData';
+
 const navMenu = document.getElementById('nav-menu');
 const toggleMenu = document.getElementById('toggle-menu');
 const closeMenu = document.getElementById('close-menu');
-
 
 // NAVBAR
 toggleMenu.addEventListener('click', () => {
@@ -24,7 +25,33 @@ closeMenu.addEventListener('click', () => {
 });
 
 closeMenu.addEventListener('keydown', () => {
-    navMenu.classList.remove('show');
+  navMenu.classList.remove('show');
 });
 
+// CARD
 
+function getCards() {
+  let cardHTML = ``
+  cardData.forEach(function (card) {
+    cardHTML += `
+    
+      <div class="cta__card">
+          <div class="cta__content">
+            <h2 class="cta__number">${card.id}</h2>
+            <h3 class="cta__title">${card.level}</h3>
+            <p class="cta__text">${card.text}
+            </p>
+            <h4 class="cta__price">Price: &euro;<span>${card.price}</span></h4>
+            <button class="cta__pay" id="cta-pay">Pay Now</button>
+          </div>
+      </div>
+    `;
+  });
+  return cardHTML
+}
+
+function render () {
+  document.getElementById('cta-container').innerHTML = getCards();
+}
+
+render()
