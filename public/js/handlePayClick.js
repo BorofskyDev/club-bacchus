79cb1f84eef5ab1b1ed4e657d63571733fb1331b
solovyovsdom <joelborofsky@mac.com>
2022-10-12 20:49:43 -0500

    refactored JS

--- public/js/handlePayClick.js
+++ public/js/handlePayClick.js
@@ -5,8 +5,8 @@ const aurBtn = document.getElementById('aur-btn');
 const varanBtn = document.getElementById('varan-btn');
 const vipBtn = document.getElementById('vip-btn');
 const payModal = document.querySelector('.pay__modal');
-const declineBtn = document.getElementById('decline-btn')
-const payBtn = document.getElementById('pay-btn')
+const declineBtn = document.getElementById('decline-btn');
+const payBtn = document.getElementById('pay-btn');
 
 let totalPrice = 0;
 
@@ -33,7 +33,7 @@ declineBtn.addEventListener('click', () => {
   paymentModal();
   modal.style.left = '-200%';
   payModal.style.right = '0';
-})
+});
 
 function secondOffer() {
   let addOn = 0;
@@ -68,11 +68,11 @@ function paymentModal() {
 
 payBtn.addEventListener('click', () => {
   document.getElementById('payment-recieved').innerHTML = ` 
-  <h2 class="thanks">Your payment has been recieved</h2>
-  <p class="thank-you">I hope you enjoyed the experience. Please do visit my 
-  actual website at <a href="https://joelborofsky.com" target="_blank">BorofskyDev</a>!</p>
-  `
-  setTimeout(function(){
+                  <h2 class="thanks">Your payment has been recieved</h2>
+                  <p class="thank-you">I hope you enjoyed the experience. Please do visit my 
+                  actual website at <a href="https://joelborofsky.com" target="_blank">BorofskyDev</a>!</p>
+  `;
+  setTimeout(function () {
     payModal.style.right = '-200%';
-  }, 5000)
-})
+  }, 5000);
+});
